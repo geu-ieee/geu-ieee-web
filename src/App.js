cdc04c75115ab106f5063731c836f03bc6807b9e
vishalk31 <vishal.k@pinaqui.com>
2022-11-04 02:19:08 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import Hero from "./Components/Home/Hero";
 import Slider from "./Components/Home/Slider";
 import Navbar from "./Components/Navbar/Navbar";
 import Topnavbar from "./Components/Navbar/Topnavbar";
-import Join from "./Components/Membership/Join"
-import Footer from "./Components/Footer/Footer"
+import Join from "./Components/Membership/Join";
+import Footer from "./Components/Footer/Footer";
 const App = () => {
   return (
     <>
       <Topnavbar />
       <Navbar />
-<<<<<<< HEAD
       <Hero />
       {/* <Slider /> */}
-=======
       <Join />
       <Footer />
->>>>>>> d8c0e037c637bc1d9bd10cf3847213ab8e39e48d
     </>
   );
 };
